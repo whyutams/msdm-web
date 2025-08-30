e3844ee18a39cff9d1cdb2f678b931c908c73ab2
ncrnt <wahyustamuu@gmail.com>
2025-08-30 23:14:12 +0800

    add middleware CheckRole

--- app/Helpers/MainHelper.php
+++ app/Helpers/MainHelper.php
@@ -1,11 +1,23 @@
 <?php
 
 if (!function_exists('format_label')) {
-    function format_label(string $text): string
+    function format_label(string|null $text): string
     {
-        return collect(explode('_', $text))
-            ->map(function ($word) {
-                return strlen($word) <= 3 ? strtoupper($word) : ucfirst($word);
+        if(!$text) return "-";
+
+        $exceptions = ['dan', 'atau', 'yang', 'dari', 'ke', 'di', 'ada'];
+
+        $words = explode('_', strtolower(trim($text)));
+
+        return collect($words)
+            ->map(function ($word, $index) use ($exceptions) {
+                if (in_array($word, $exceptions)) {
+                    return $index === 0 ? ucfirst($word) : $word;
+                }
+
+                return strlen($word) <= 3
+                    ? strtoupper($word)
+                    : ucfirst($word);
             })
             ->join(' ');
     }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class,
+        'check.biodata' => \App\Http\Middleware\CheckBiodata::class,
+
     ];
 }
