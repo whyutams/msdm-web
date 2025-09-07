6a9112af01e1a9982788494762ba662cb90d0252
ncrnt <wahyustamuu@gmail.com>
2025-09-07 11:46:40 +0800

    ganti summernote jadi quill editor

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -24,13 +24,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Summernote CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+    <!-- Quill CSS -->
+    <link href="https://cdn.quilljs.com/1.3.7/quill.snow.css" rel="stylesheet">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
-    <!-- Summernote JS -->
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
 </head>
 
 <body class="bg-white text-gray-800">
@@ -41,27 +39,55 @@
     @include('components/footer')
 
     <script src="https://cdn.jsdelivr.net/npm/cropperjs@1.6.2/dist/cropper.min.js"></script>
+    <!-- Quill JS -->
+    <script src="https://cdn.quilljs.com/1.3.7/quill.min.js"></script>
     <script>
-        $(function () {
-            // Summernote
-            const summernote_ids = ['summernote', 'summernote2'];
+        $(document).ready(function () {
+            // Quill Editor
+            const quillEditors = ['summernote', 'summernote2'];
+            quillEditors.forEach(id => {
+                const quillContainer = document.getElementById(id);
 
-            summernote_ids.forEach((id, i) => {
-                $(`#${id}`).summernote({
-                    height: 350,
-                    toolbar: [
-                        ['style', ['style']],
-                        ['font', ['bold', 'italic', 'underline']],
-                        ['fontname', ['fontname']],
-                        ['fontsize', ['fontsize']],
-                        ['color', ['color']],
-                        ['para', ['ol', 'ul', 'paragraph', 'height']],
-                        ['view', ['undo', 'redo', 'fullscreen']]
-                    ],
-                    callbacks: {}
-                });
-            })
-        }); 
+                if (quillContainer) {
+                    const parent = quillContainer.parentNode;
+                    const quillDiv = document.createElement('div');
+                    quillDiv.id = id + '-quill';
+                    quillDiv.className = 'bg-white h-64 rounded-lg border';
+                    quillDiv.innerHTML = quillContainer.value;
+                    parent.replaceChild(quillDiv, quillContainer);
+
+                    const hiddenInput = document.createElement('input');
+                    hiddenInput.type = 'hidden';
+                    hiddenInput.name = quillContainer.name;
+                    hiddenInput.id = quillContainer.id;
+                    parent.appendChild(hiddenInput);
+
+                    const quill = new Quill(quillDiv, {
+                        theme: 'snow',
+                        modules: {
+                            toolbar: [
+                                [{ 'font': [] }, { 'size': [] }],
+                                ['bold', 'italic', 'underline', 'strike'],
+                                [{ 'color': [] }, { 'background': [] }],
+                                [{ 'script': 'super' }, { 'script': 'sub' }],
+                                [{ 'header': '1' }, { 'header': '2' }, 'blockquote', 'code-block'],
+                                [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+                                ['direction', { 'align': [] }],
+                                ['link'],
+                                ['clean']
+                            ]
+                        },
+                        placeholder: 'Tulis sesuatu...'
+                    });
+
+                    // submit form
+                    const form = quillDiv.closest('form');
+                    form.addEventListener('submit', function () {
+                        hiddenInput.value = quill.root.innerHTML;
+                    });
+                }
+            });
+        });
     </script>
 
 </html>
